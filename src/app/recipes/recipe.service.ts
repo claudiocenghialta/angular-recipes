76c9e46a4735e22b9408847e9ecaa8d0abcf148f
Claudio Cenghialta <62311968+claudiocenghialta@users.noreply.github.com>
2021-10-18 17:58:48 +0200

    recipe service for selected recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,11 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>()
+
+
    private recipes: Recipe[] = [
     new Recipe('a test recipe', 'this is the description', 'https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg'),
     new Recipe('a new recipe ', 'description', 'https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg')
