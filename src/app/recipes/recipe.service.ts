a425561d0900b8dfd9b719f08f6ac975f6364750
Claudio Cenghialta <62311968+claudiocenghialta@users.noreply.github.com>
2021-10-20 16:12:38 +0200

    shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,44 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
-
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>()
 
 
    private recipes: Recipe[] = [
-    new Recipe('a test recipe', 'this is the description', 'https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg'),
-    new Recipe('a new recipe ', 'description', 'https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg')
+     new Recipe(
+       'a test recipe',
+      'this is the description',
+       'https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg',
+       [
+         new Ingredient('Carne', 1),
+         new Ingredient('Patate',30)
+      ]
+    ),
+     new Recipe(
+       'a new recipe ',
+      'description',
+       'https://p1.pxfuel.com/preview/4/17/111/recipe-comisa-healthy-tomato.jpg',
+       [
+         new Ingredient('Pesce', 5),
+         new Ingredient('Spinaci',10)
+       ]
+     )
    ];
   
+  constructor(private slService: ShoppingListService) {
+    
+  }
+
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients :Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
